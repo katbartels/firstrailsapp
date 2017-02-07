5793315269e61ebb8dff8945bbd889e52c0fbb22
Katrin Bartels <katrin.bartels@gmail.com>
2017-02-07 10:32:53 +0100

    5.8 added comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,20 @@ Rails.application.routes.draw do
   get 'static_pages/landing_page'
 
   resources :products
-  get 'static_pages/about'
 
-  get 'static_pages/contact'
+    get 'static_pages/about'
 
-  post 'static_pages/thank_you'
+    get 'static_pages/contact'
 
-  root 'static_pages#landing_page'
+    post 'static_pages/thank_you'
 
-  resources :orders, only: [:index, :show, :create, :destroy]
+    root 'static_pages#landing_page'
 
-    
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :orders, only: [:index, :show, :create, :destroy]
+
+resources :products do
+  resources :comments
+end
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
