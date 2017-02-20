eff64cc14430f20602d2fea63d4bb21966eb55c3
Katrin Bartels <katrin.bartels@gmail.com>
2017-02-20 18:10:25 +0100

    6.2 Unit Tests

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,16 @@
+
+require 'rails_helper'
+
+describe Order do
+
+  context "Empty total" do
+
+    it "is not valid" do
+
+      expect(Order.new(total:nil)).not_to be_valid
+
+    end
+
+  end
+
+end
